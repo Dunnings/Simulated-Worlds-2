0f8966f57984397a118654f1db32866001a3a4ef
David Dunnings <David2.Dunnings@live.uwe.ac.uk>
2015-03-23 01:25:04 +0000

    .

--- PredPreyBoids/Game/gamedata.h
+++ PredPreyBoids/Game/gamedata.h
@@ -3,6 +3,7 @@
 #include "gamestate.h"
 #include <dinput.h>
 
+class UserInterfaceManager;
 class MyEffectFactory;
 
 struct GameData
@@ -11,9 +12,11 @@ struct GameData
 	unsigned char* keyboard; //current keyboard state
 	unsigned char* prevKeyboard; //previous frame's keyboard state
 	DIMOUSESTATE* mouse; //current mouse state
+	DIMOUSESTATE* prevMouse; //previous mouse state
 	GameState GS; //current game state
 	static ID3D11Device* p3d;
 	static MyEffectFactory* EF;
+	UserInterfaceManager* UIManager;
 };
 
 struct SimulationParameters
